d760e8047c7bd9e67f62bdc12f840d730a1b8a02
veera pulapakura <veera_ratna@yahoo.com>
2024-05-19 19:17:06 +0100

    Updated Xpath parameterisation.

--- src/test/java/starter/actions/FinancialServicesSteps.java
+++ src/test/java/starter/actions/FinancialServicesSteps.java
@@ -5,26 +5,25 @@ import net.serenitybdd.core.steps.UIInteractionSteps;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import starter.pageobjects.FinancialServicesPage;
 import starter.pageobjects.Homepage;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
 
 public class FinancialServicesSteps extends UIInteractionSteps {
+    FinancialServicesPage financialServicesPage;
     WebDriver driver;
-
     public void verify_financial_srvices_titles() {
         String listofvalues = find(Homepage.LIST_UNDER_FINANCIALSERVICES).getText();
         List<String> arrayList = new ArrayList<String>(Arrays.asList(listofvalues.split("\n")));
-             for (int i=0;i<arrayList.size();i++) {
+
+        for (int i=0;i<arrayList.size();i++) {
                  String value = arrayList.get(i);
-                 By elementvalue= By.xpath("//a[@href='/financial-services/"+value+"']//div[@class='subpage-title'][normalize-space()='"+value+"']");
-                 withTimeoutOf(Duration.ofSeconds(10));
+                 By elementvalue = (By) financialServicesPage.commonxpath(driver,value );
                  assertEquals(true,find(elementvalue).isPresent());
                  assertEquals(true,find(elementvalue).isClickable());
              }
--- src/test/java/starter/pageobjects/FinancialServicesPage.java
+++ src/test/java/starter/pageobjects/FinancialServicesPage.java
@@ -1,13 +1,14 @@
 package starter.pageobjects;
 import net.serenitybdd.core.pages.PageObject;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
 
 public class FinancialServicesPage extends PageObject {
-    public static final By FINANCIAL_CRIME_COMPLIANCE = By.className("Financial Crime Compliance");
-    public static final By FRAUD_AND_IDENTITY_MANAGEMENT = By.className("Fraud and Identity Management");
-    public static final By CUSTOMER_DATA_MANAGEMENT = By.className("Customer Data Management");
-    public static final By CREDIT_RISK_MANAGEMENT = By.className("Credit Risk Assessment");
-    public static final By COLLECTIONS_AND_RECOVERY = By.className("Collections and Recovery");
-    public static final By INVESTIGATIONS_AND_DUEDELIGENCE = By.className("Investigations and Due Diligence");
-    public static final By RISK_ORCHESTRATION = By.className("Risk Orchestration");
+    public By commonxpath(WebDriver driver, String xpathvalue){
+        By elementvalue= By.xpath("//a[@href='/financial-services/"+xpathvalue+"']//div[@class='subpage-title'][normalize-space()='"+xpathvalue+"']");
+        withTimeoutOf(Duration.ofSeconds(10));
+        return elementvalue;
+    }
 }
